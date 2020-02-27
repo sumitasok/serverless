66511eab641c1e24774f215e3d16d460d747f6db
Sumit Asok <sumitasok@gmail.com>
2020-02-27 13:25:39 +0530

    https://serverless.com/blog/serverless-python-packaging/ until npm init

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-setup:
+setup: venv_build
 	npm install -g serverless
 
 create:
 	serverless create --template aws-python3 --name numpy-test --path numpy-test
+
+venv_build:
+	docker-compose -f "docker-compose.yml" up -d --build 
--- numpy-test/handler.py
+++ numpy-test/handler.py
@@ -1,24 +1,10 @@
-import json
+import numpy as np
 
+def main(event, context):
+    a = np.arange(15).reshape(3, 5)
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    print("Your numpy array:")
+    print(a)
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+if __name__ == "__main__":
+    main('', '')
